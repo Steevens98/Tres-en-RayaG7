c1d19af3c670b079b9203c2b606ce81c0cb1129c
IsaacVra <isakvera@espol.edu.ec>
2025-08-10 10:33:08 -0500

    Creacion de la clase Tree

---
+++ TresEnRayaG7/src/main/java/com/mycompany/TDAs/Tree.java
@@ -0,0 +1,59 @@
+package com.mycompany.TDAs;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tree<E> {
+    private TreeNode<E> root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+    public Tree(Jugada[][] m1) {
+        TreeNode matriz = new TreeNode<>(m1);
+        this.root = matriz;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public Jugada[][] getRoot() {
+        return root.getContent();
+    }
+
+    public TreeNode<E> getRootNode() {
+        return this.root;
+    }
+
+    public void setRoot(Jugada[][] matriz) {
+        if (this.root == null) {
+            this.root = new TreeNode<>(matriz);
+        } else {
+            this.root.setContent(matriz);
+        }
+    }
+
+    public boolean isLeaf() {
+        return this.root.getChildren().isEmpty();
+    }
+
+    public void tablero() {
+        Integer[][] tabl= new Integer[3][3];
+
+        for (Integer[] ta : tabl) {
+            for (int j = 0; j < ta.length; j++) {
+                ta[j] = -1;
+            }
+        }
+       
+    }
+
+    public void addChildren(Tree<Jugada[][]> children) {
+        if (this.root != null) {
+            this.root.addChildrenNode(children);
+        }
+    }
+}
