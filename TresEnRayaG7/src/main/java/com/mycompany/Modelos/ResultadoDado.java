c69e18a3115b06f4473f78fadf0bcbfb97d389c5
Gustavo Moscoso <gustavomoscoso2004@gmail.com>
2025-08-10 13:52:49 -0500

    Creacion de la clase DadoResultado

---
+++ TresEnRayaG7/src/main/java/com/mycompany/Modelos/ResultadoDado.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Modelos;
+
+/**
+ *
+ * @author gutav
+ */
+public class ResultadoDado {
+    private String lado;
+    private int numero;
+
+    public ResultadoDado(String lado, int numero) {
+        this.lado = lado;
+        this.numero = numero;
+    }
+
+    public String getLado() {
+        return lado;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+    
+}
